c4a3125e6a3f7a3cdf9dc6fde7b450194dcaf0fb
croookid <102533229+croookid@users.noreply.github.com>
2022-05-03 12:31:03 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 2) Image Slider
+// Image Slider
 
 var slider = document.querySelector('#slider');
 var slides = slider.querySelector('.slides');
@@ -22,3 +22,29 @@ setInterval(function() {
         currentSlide = 0;
     }
 }, 1000);
+
+
+// top button
+const pageTop = document.querySelector('#pageTop')
+window.addEventListener("scroll", function(){
+    let y = window.pageYOffset;
+    if(y > 150){
+      pageTop.style.opacity = '1';
+      pageTop.style.bottom = '15px';
+    }else{
+      pageTop.style.opacity = '0';
+      pageTop.style.bottom = '-58px';
+    }
+  })
+
+// qbn button
+
+const qnb = document.getElementById('qnb')
+window.addEventListener("scroll", function(){
+    let y = Number(window.pageYOffset);
+    if(y > 470){
+        qnb.style.top = y  + 'px';
+    }else{
+        qnb.style.top = '512px';
+    }
+  })
