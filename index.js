aaa76b8dbf49c78bff6bd541e61b38410cee484a
croookid <102533229+croookid@users.noreply.github.com>
2022-05-09 21:38:59 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,124 @@
 // Image Slider
 
+
 var slider = document.querySelector('#slider');
-var slides = slider.querySelector('.slides');
+var slides = document.querySelector(".slides")
 var slide = slides.querySelectorAll('.slide');
+var sliderButton = document.querySelectorAll('.slider-button')
+
 
 var currentSlide = 0;
+var currentPx = 0
+document.querySelector('.slider-numbering').innerHTML = `${slide.length-1}/${slide.length-1}`
 
-setInterval(function() {
-    var from = -(innerWidth * currentSlide);
-    var to = from - innerWidth;
+//rightslider
+let imgSlider = function() {
+  currentPx = slide[0].scrollWidth 
+  var from = -(currentPx * (currentSlide));
+  var to = from - currentPx;
+  slides.animate({
+      marginLeft: [from + "px", to + "px"]
+  }, {
+      duration: 500,
+      easing: "ease",
+      iterations: 1,
+      fill: "both"
+  });
+  currentSlide++;
+  document.querySelector('.slider-numbering').innerHTML = `${currentSlide}/${slide.length-1}`
+  if (currentSlide === (slide.length-1)) {
+      currentSlide = 0;
+  }
+  
+}
+
+//leftslider
+let imgSliderLeft = function() {
+  currentPx = slide[0].scrollWidth 
+  var from = -(currentPx * (currentSlide));
+  var to = from + currentPx;
+  slides.animate({
+      marginLeft: [from + "px", to + "px"]
+  }, {
+      duration: 500,
+      easing: "ease",
+      iterations: 1,
+      fill: "both"
+  });
+  currentSlide--;
+  if (currentSlide === (0)) {
+    currentSlide = slide.length-1;
+}
+  document.querySelector('.slider-numbering').innerHTML = `${currentSlide}/${slide.length-1}`
+
+
+  
+}
+
+//interval
+let interval = setInterval(imgSlider, 1000);
+slider.addEventListener('mouseover', function(){
+  clearInterval(interval)
+  sliderButton.forEach((e)=>{
+    e.style.opacity = 1;
+  })
+})
+slider.addEventListener('mouseout', function(){
+  interval = setInterval(imgSlider, 1000)
+  sliderButton.forEach((e)=>{
+    e.style.opacity = 0;
+  })
+})
+sliderButton[0].addEventListener('click', imgSliderLeft)
+sliderButton[1].addEventListener('click', imgSlider)
+
+let pressed = false;
+let startx;
+let startmargin
+let x;
+let currentMarginLeft;
+
+slider.addEventListener('mousedown', (e)=>{
+  pressed = true;
+  startmargin = window.getComputedStyle(document.querySelector(".slides")).marginLeft;
+  startx = e.offsetX
+})
+
+window.addEventListener('mouseup', ()=>{
+  pressed = false;
+})
+
+slides.addEventListener('mousemove', (e)=>{
+  if(!pressed) return;
+  x = e.offsetX 
+  currentMarginLeft = parseInt(startmargin, 10) + (x-startx)
+  slides.animate({
+    marginLeft: [startmargin , currentMarginLeft + "px"]
+}, {
+    duration: 0.00000000000000001,
+    iterations: 1,
+    fill: "both"
+})
+})
+slides.addEventListener('mouseup', (e)=>{
+  console.log(1)
+  if ((x-startx) > (parseInt(currentPx, 10)) * 0.1){
+    imgSliderLeft();
+  }
+  if (-(x-startx) > parseInt(currentPx, 10) * 0.1){
+    imgSlider();
+  }
+  if (Math.abs(x-startx) < (parseInt(currentPx, 10)) * 0.1){
     slides.animate({
-        marginLeft: [from + "px", to + "px"]
-    }, {
-        duration: 500,
-        easing: "ease",
-        iterations: 1,
-        fill: "both"
-    });
-    currentSlide++;
-    if (currentSlide === (slide.length - 1)) {
-        currentSlide = 0;
-    }
-}, 1000);
-
+      marginLeft: [currentMarginLeft + "px" , startmargin]
+  }, {
+      duration: 0.00000000000000001,
+      iterations: 1,
+      fill: "both"
+  })
+  }
+} 
+)
 
 // top button
 const pageTop = document.querySelector('#pageTop')
